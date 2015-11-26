aeea05b50904ad47ae390ca94f67f9cfc50a5ab4
1079354 <1079354@22HW010740.SOAM.TCS.com>
2015-11-26 17:25:50 -0600

    Se agrega sql del portal y jar de complemento

---
+++ src/java/Test/Sql.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+/**
+ *
+ * @author 1079354
+ */
+public class Sql {
+    /*
+    CREATE DATABASE News;
+ USE News;
+
+	CREATE TABLE usuarios(
+	id_usuario INT(5) NOT NULL AUTO_INCREMENT,
+    usuario VARCHAR(30) NOT NULL,
+    contrasenia VARCHAR(30) NOT NULL,
+    nombre VARCHAR(30) NOT NULL,
+    apellidos VARCHAR(40) NOT NULL,
+    tipo CHAR(1) NOT NULL,
+    estatus VARCHAR(2) NOT NULL,
+    PRIMARY KEY(id_usuario)
+	);
+    
+    INSERT INTO usuarios VALUES(1,'fjuarez','123','Francisco Pedro','Juárez Medrano','a',1);	
+    INSERT INTO usuarios VALUES(2,'gperez','1234','Gustavo','Perez Savedra','o',1);
+    INSERT INTO usuarios VALUES(3,'elópez','1234','Edgar','López Jimenez','o',0);
+	
+	
+	CREATE TABLE componentes(
+	id_componente INT(5) NOT NULL AUTO_INCREMENT,
+	titulo VARCHAR(30) NOT NULL,
+	usuario VARCHAR(30) NOT NULL,
+	alias_img VARCHAR(30) NOT NULL,
+	desc_componente VARCHAR(5000) NOT NULL,
+	estatus INT(1) NOT NULL,
+	genero VARCHAR(20) NOT NULL,
+	url_video VARCHAR(60) NOT NULL,
+	fecha DATE,
+	PRIMARY KEY(id_componente)
+	);
+	
+	INSERT INTO componentes VALUES(1, 'El Gran Silencio', 'fjuarez', 'elgransilencio', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales.', 1, 'rock', 'http://mx.youtube.com/watch?v=LsSZal2-5y...', '2015-11-26');
+	INSERT INTO componentes VALUES(2, 'Madnnes', 'fjuarez', 'madnnes', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales.', 1, 'rock', 'http://mx.youtube.com/watch?v=LsSZal2-5y...', '2015-11-26');
+	INSERT INTO componentes VALUES(3, 'Sex Pistols', 'fjuarez', 'sexpistols', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales.', 1, 'rock', 'http://mx.youtube.com/watch?v=LsSZal2-5y...', '2015-11-26');
+    */
+}
